cdc86e0faba82b16f6f6c27e47ff488d76c1b482
zhengyy <869601606@qq.com>
2019-08-23 17:24:35 +0800

    8.23

--- BWYSDP/Controls/FromSourceProperty.cs
+++ BWYSDP/Controls/FromSourceProperty.cs
@@ -14,6 +14,7 @@ namespace BWYSDP.Controls
 {
     public partial class FromSourceProperty : BaseUserControl<LibFromSourceField>
     {
+        LibDataSource _ds = null;
         public FromSourceProperty()
         {
             InitializeComponent();
@@ -34,16 +35,16 @@ namespace BWYSDP.Controls
         {
             Control ctl = sender as Control;
             string ctrNm = ctl.Name.Replace(SysConstManage.BtnCtrlNmPrefix, "");
+            Panel p = new Panel();
+            p.Dock = DockStyle.Fill;
+            p.Name = "pfieldcollection";
+            p.AutoScroll = true;
+            ListBox listBox = new ListBox();
+            listBox.Dock = DockStyle.Fill;
+            p.Controls.Add(listBox);
             if (string.Compare(ctrNm, "fsfield_FromDataSource") == 0)//来源数据源
             {
                 string[] dsarray = ModelManager.GetAllDataSourceNm(string.Empty);
-                Panel p = new Panel();
-                p.Dock = DockStyle.Fill;
-                p.Name = "pfieldcollection";
-                p.AutoScroll = true;
-                ListBox listBox = new ListBox();
-                listBox.Dock = DockStyle.Fill;
-                p.Controls.Add(listBox);
                 if (dsarray != null && dsarray.Length > 0)
                 {
                     foreach (string item in dsarray)
@@ -55,20 +56,85 @@ namespace BWYSDP.Controls
                 DialogResult dialog = fielsform.ShowDialog(this);
                 if (dialog == DialogResult.OK)
                 {
-
+                    if (this.Controls[ctrNm].Text.Trim() != listBox.SelectedItem.ToString())
+                    {
+                        foreach (Control c in this.Controls)
+                        {
+                            if (!c.GetType().Equals(typeof(Label)))
+                                c.Text = string.Empty;
+                        }
+                        this.Controls[ctrNm].Text = listBox.SelectedItem.ToString();
+                        //this.Controls["fsfield_FromDefindTableNm"].Text = string.Empty;
+                        //this.Controls["fsfield_FromStructTableNm"].Text = string.Empty;
+                        //this.Controls["fsfield_FromFieldNm"].Text = string.Empty; 
+                    }
                 }
             }
             else if (string.Compare(ctrNm, "fsfield_FromDefindTableNm") == 0)//来源自定义表名
             {
-
+                if (!string.IsNullOrEmpty(this.Controls["fsfield_FromDataSource"].Text))
+                {
+                    this._ds =ModelManager .GetDataSource(this.Controls["fsfield_FromDataSource"].Text);
+                    if (this._ds != null&& this._ds .DefTables !=null)
+                    {
+                        foreach (LibDefineTable deftb in this._ds.DefTables)
+                        {
+                            listBox.Items.Add(deftb.TableName);
+                        }
+                    }
+                }
+                FieldCollectionForm fielsform = new FieldCollectionForm(p);
+                DialogResult dialog = fielsform.ShowDialog(this);
+                if (dialog == DialogResult.OK)
+                {
+                    this.Controls[ctrNm].Text = listBox.SelectedItem.ToString();
+                }
             }
             else if (string.Compare(ctrNm, "fsfield_FromStructTableNm") == 0)//来源数据表名
             {
+                if (!string.IsNullOrEmpty(this.Controls["fsfield_FromDefindTableNm"].Text))
+                {
+                    if(_ds==null)
+                        _ds= ModelManager.GetDataSource(this.Controls["fsfield_FromDataSource"].Text);
+                    LibDefineTable deftb = this._ds.DefTables.FindFirst("TableName", this.Controls["fsfield_FromDefindTableNm"].Text.Trim());
+                    if (deftb != null)
+                    {
+                        foreach (LibDataTableStruct dt in deftb.TableStruct)
+                        {
+                            listBox.Items.Add(dt.Name);
+                        }
+                    }
 
+                }
+                FieldCollectionForm fielsform = new FieldCollectionForm(p);
+                DialogResult dialog = fielsform.ShowDialog(this);
+                if (dialog == DialogResult.OK)
+                {
+                    
+                }
             }
             else if (string.Compare(ctrNm, "fsfield_FromFieldNm") == 0)//来源字段
             {
+                if (!string.IsNullOrEmpty(this.Controls["fsfield_FromStructTableNm"].Text))
+                {
+                    if (_ds == null)
+                        _ds = ModelManager.GetDataSource(this.Controls["fsfield_FromDataSource"].Text);
+                    LibDefineTable deftb = this._ds.DefTables.FindFirst("TableName", this.Controls["fsfield_FromDefindTableNm"].Text.Trim());
+                    LibDataTableStruct dtstruct = deftb.TableStruct.FindFirst("Name", this.Controls["fsfield_FromStructTableNm"].Text.Trim());
+                    if (dtstruct != null)
+                    {
+                        foreach (LibField f in dtstruct.Fields)
+                        {
+                            listBox.Items.Add(f.Name);
+                        }
+                    }
+                }
+                FieldCollectionForm fielsform = new FieldCollectionForm(p);
+                DialogResult dialog = fielsform.ShowDialog(this);
+                if (dialog == DialogResult.OK)
+                {
 
+                }
             }
         }
     }
